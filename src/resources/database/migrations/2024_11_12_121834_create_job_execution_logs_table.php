be50c57faa9849fab03b800eb1c5eccd828e986c
Attila Fulop <1162360+fulopattila122@users.noreply.github.com>
2024-11-28 18:14:58 +0200

    Added cascade on delete to job execution logs

--- src/resources/database/migrations/2024_11_12_121834_create_job_execution_logs_table.php
+++ src/resources/database/migrations/2024_11_12_121834_create_job_execution_logs_table.php
@@ -11,7 +11,7 @@ return new class () extends Migration {
     {
         Schema::create('job_execution_logs', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('job_execution_id')->constrained();
+            $table->foreignId('job_execution_id')->constrained()->onDelete('cascade');
             $table->timestamp('happened_at');
             $table->string('level', 9);
             $table->text('message');
